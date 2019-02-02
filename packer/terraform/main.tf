94f2f8d2e33fab6415a8dc42fdb3e0db50955475
nonamed <123anice@mail.ru>
2019-02-03 01:16:16 +0300

    Add terraform config

---
+++ packer/terraform/main.tf
@@ -0,0 +1,57 @@
+provider "google" {
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+
+  # определение загрузочного диска
+  tags = ["reddit-app"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  # определение сетевого интерфейса
+  tags = ["reddit-app"]
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  metadata {
+    ssh-keys = "Buldozer:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с тегом …
+  target_tags = ["reddit-app"]
+}
